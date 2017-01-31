08f179399c35875a633845acd7f5d2d2e2b92485
Bob Habig <rfhab2@gmail.com>
2017-01-31 16:01:41 -0500

    sessions controller done. users create done. a few helpers in application and users controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    @user ||= User.find_by(id: session[:user_id])
+  end
+
+
+  def password?
+    params[:user][:password] || !params[:user][:password].nil?
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  before_action :logged_in?, only: [:edit, :update, :show, :index, :destroy]
+  before_action :set_current_user, only: [:edit, :update, :show, :index, :destroy]
+  
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if password? && matching_password?
+      @user = User.create(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        redirect_to new_user_path, alert: "There was a problem creating an account with the information provided. Please try again"
+      end
+    else
+      redirect_to new_user_path, alert: "Passwords did not match. Please re-enter"
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+
+  end
+
+  private
+
+  def matching_password?
+    params[:user][:password] == params[:user][:password_confirmation]
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_comfirmation)
+  end
+end
