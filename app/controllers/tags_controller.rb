c946d4cb24dcdbbd686d6bbe1c7c87b1d196d52a
Bob Habig <rfhab2@gmail.com>
2017-01-31 23:28:47 -0500

    tag controller stubbed out

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,11 @@
 class TagsController < ApplicationController
+  before_action :logged_in?
+  before_action :current_user
+  def index
+    @tags = Tag.all
+  end
 
+  def show
+    @tag = Tag.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#hello'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :tags
-  resources :posts
+  
+  resources :posts do
+    resources :tags, only: [:index, :show]
+  end
+
   resources :users
 end
