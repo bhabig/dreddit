cb4789bd7d234fe9cd784007c60dea7ce5799e46
Bob Habig <rfhab2@gmail.com>
2017-02-01 12:28:42 -0500

    empty actions required for posts and tags controllers. partially complete post_params (will need to include attributes method to allow tag creation in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,44 @@
 class PostsController < ApplicationController
+  before_action :logged_in?
+  before_action :current_user
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+    @post.tags.build
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :title)
+  end
+
+  def set_post
+    @post ||= Post.find_by(id: params[:id])
+  end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,7 @@
 class TagsController < ApplicationController
   before_action :logged_in?
   before_action :current_user
+  
   def index
     @tags = Tag.all
   end
